d012b3ab9c86bcac5a11ad45a709c6f0dcf2ed73
Mike <zhangrunwei@aliyun.com>
2019-04-11 18:53:55 +0800

    first commit

---
+++ Readme.txt
@@ -0,0 +1,4 @@
+mybatis逆向工程命令行方式生成代码
+一、配置custom.properties
+二、运行 MysqlRun 或者 OracleRun
+二、导出到项目中如果乱码，要把eclipse的编码设为UTF-8
---
+++ app/RunMsql.bat
@@ -0,0 +1,1 @@
+java -Dfile.encoding=UTF-8 -cp .;my-comment-generator.jar;mybatis-generator-core-1.3.7.jar org.mybatis.generator.api.ShellRunner -configfile GeneratorConfig.xml -overwrite -verbose -contextids mysql
